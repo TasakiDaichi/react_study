d118282ec9198f06ba1dec553c3d0a2c7ac327f4
TasakiDaichi <d.tasaki1212@icloud.com>
2025-08-24 15:57:01 +0900

    import追加

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import MovieDetail from './MovieDetail.tsx'
+import MovieDetail from './MovieDetali/MovieDetail.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Header from './Header.tsx'
 
